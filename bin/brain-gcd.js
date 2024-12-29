78a9f6e2a315e00dd7a7b24df5d4cfd8797b169d
Polina Kasich <poli.key@yandex.ru>
2024-12-29 18:46:33 +0500

    add game-gcd and brain-gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import { index } from "../src/index.js";
+import { gameGcd } from "../games/game-gcd.js";
+
+
+// вызывается интерфейс для игры Brain-Even
+// передаются данные об игре: правила игры, числа для определения четности, правильные ответы
+index(gameGcd());
---
+++ games/game-gcd.js
@@ -0,0 +1,33 @@
+import {gameDataGen} from "./game-layout.js";
+
+export const gameGcd = () => {
+    // сборка данных для игры
+    const rules = "";
+
+    // вызов функции-интерфейса для создания данных в формате объекта
+    const gameGcdData = gameDataGen(rules, genData);
+
+    return gameGcdData;
+}
+
+// генерация чисел для игры
+const genData = () => {
+    const num1 = Math.round(Math.random() * 100);
+    const num2 = Math.round(Math.random() * 100);
+    let rightAnswer = euclideanAlgorithm(num1, num2);
+
+    return {expression: `${num1} ${num2}`, rightAnswer: rightAnswer};
+}
+
+// алгоритм вычисления НОД
+const euclideanAlgorithm = (num1, num2) => {
+    let a = num1;
+    let b = num2;
+
+    while (a !== b) {
+        if (a > b) a = a - b;
+        else b = b - a;
+    }
+
+    return a;
+}
